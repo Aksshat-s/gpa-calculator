3675c434c7efdec31629a9e7502dbbf1898e9b18
Aksshat-s <108083985+Aksshat-s@users.noreply.github.com>
2025-11-04 15:52:00 -0500

    Update main.py and added title

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ Ask user for number of classes
 Ask for grades and course weights
 Calculate Unweighted and weighted GPA
 Display Results Clearly
+
+# ===================================
+Start of GPA Calculator code
+# ===================================
+
+print("=== GPA Calculator ===/n")
